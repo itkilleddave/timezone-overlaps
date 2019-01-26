2ee0673304f3d83819983aa238ef3d23e051ea55
Dave Longbottom <davelongbottom@gmail.com>
2019-01-26 15:12:41 +1030

    hello from express test working

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,33 @@ import './App.css';
 import TimezoneApp from './components/TimezoneApp';
 
 class App extends Component {
+
+
+  //// express test /////////////////////
+
+  state = {
+    response: '',
+    post: '',
+    responseToPost: '',
+  };
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
+  ///////////////////////////////////////
+
   render() {
     return (
       <div className="app">
+      	<h1>{this.state.response}</h1> {/*express test*/}
         <TimezoneApp />
       </div>
     );
